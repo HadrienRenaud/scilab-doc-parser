cd02a1f6022c09140a9b36039948d3354182273b
HadrienRenaudLebret <hadrien.renaudlebret@gmail.com>
2017-12-20 23:07:34 +0100

    Ajout de fileParse

--- parser.py
+++ parser.py
@@ -13,8 +13,11 @@ import re
 # Regex
 #=======================================================================================================================
 
+
+RE_CONTINUATION = re.compile(r"\s*\.\.\s*")
 RE_FUN_DECLARATION = re.compile(r"^function (?P<syntax>(\[\s*(?P<arg_out>(\w+,\s*)*\w+)\s*\]\s*=\s*)?(?P<fun_name>\w+)\s*\(\s*(?P<arg_in>(\w+,\s*)*\w*)\s*\))\s*$")
 RE_DOCSTRING = re.compile(r"$\s+//.*")
+RE_FUN_END = re.compile(r"endfunction\s*")
 
 
 #=======================================================================================================================
@@ -29,18 +32,69 @@ RE_DOCSTRING = re.compile(r"$\s+//.*")
 # Function fileParse
 #=======================================================================================================================
 
-def fileParse(source, options):
+def fileParse(source, dest=None, options):
     """Parse un fichier.
-        :param source: 
+        :param source: le path vers le fichier source.
+        :type source: str
+        
+        :param options: les options pour le parsage
+        :type options: dictionary
+        
+        :param dest: le path vers le fichier destination.
+            Par défaut, si source vaut ``/path/to/sourceDir/source.sci`` alors dest vaut 
+            ``/path/to/sourceDir/source.rst``.
+        :type dest: str
+        
+        Parse un fichier.
     """"
     
+    # Valeur par défaut de dest
+    if (dest is None):
+        dest = source[:-3] + "rst"
+    
+    # Lecture du fichier source
+    with open(source, "r") as fSource:
+        content = fSource.read()
+    
+    # On récupère ses lignes
+    lines = parseContinuations(content).split("\n")
+    
+    # On initialise les positions du début et de la fin de chaque fonction
+    indexes_b = []
+    indexes_e = []
+    
+    # On récupère le début et la fin de chaque fonction
+    for i,l in enumerate(lines):
+        if (RE_FUN_DECLARATION.fullmatch(l) is not None):
+            indexes_b.append(i)
+        elif (RE_FUN_END.fullmatch(l) is not None):
+            indexes_e.append(i)
+    
+    # On parse chaque fonction
+    res = []
+    for begin, end in zip(indexes_b, indexes_e):
+        res += funParse(lines[begin:end])    
+    
+    # On écrit dans le fichier destination
+    with open(dest, "w") as fDest:
+        for l in res:
+            fDest.write(l + "\n")
+
+
+# Fonction auxilliaire        
+def parseContinuations(content):
+    """Replace the continuation marks by spaces."""
+    return RE_CONTINUATION.sub(" ", content)
+
+
+
 #=======================================================================================================================
 # Function funParse
 #=======================================================================================================================
 
 
 def funParse(source, options):
-    """Parse une fonction.
+    """
         :param source: la fonction en question
         :type source: list of strings
         
@@ -49,6 +103,8 @@ def funParse(source, options):
         
         :return: la documentation de la fonction
         :rtype: list of strings
+        
+       Parse une fonction. 
     """
     
     # On commence par récupérer la docstring et la déclaration de la fonction
@@ -58,14 +114,14 @@ def funParse(source, options):
     test_re_fun_declaration = False
     test_re_docstring = True
     
-    while (i < source.length() and not test_re_fun_declaration):
+    while (i < len(source) and not test_re_fun_declaration):
         if (RE_FUN_DECLARATION.match(source[i]) is not None):
             test_re_fun_declaration = True
             docString.append(l)
         else:
             i = i + 1
     
-    while (i < source.length() and test_re_docstring):
+    while (i < len(source) and test_re_docstring):
         if (RE_DOCSTRING.match(l) is not None):
             i = i + 1
             docString.append(l)
